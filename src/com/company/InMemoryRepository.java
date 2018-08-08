e5e9b6ece72fb81d3975134a4d6e260033d567fb
Jennie Ahlberg <36049454+Jennieahlberg@users.noreply.github.com>
2018-08-08 11:28:34 +0200

    Minor chnges

--- src/com/company/InMemoryRepository.java
+++ src/com/company/InMemoryRepository.java
@@ -11,8 +11,13 @@ public class InMemoryRepository implements Repository {
 
     public Order getOrder(int orderID, int customerID) {
         for (var order : orders) {
-            if (order.customerID == customerID && order.orderID == orderID) {
-                return order;
+
+            try {
+                if (order.customerID == customerID && order.orderID == orderID) {
+                    return order;
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("OrderID: " + orderID + " for CustomerID: " + customerID + " was not found");
             }
         }
         return null;
@@ -33,15 +38,30 @@ public class InMemoryRepository implements Repository {
 
     public void deleteOrder(int orderID, int customerID) {
         for (var order : orders) {
-            if (order.customerID == customerID && order.orderID == orderID) {
-                orders.remove(order);
-                return;
+            try {
+                if (order.customerID == customerID && order.orderID == orderID) {
+                    orders.remove(order);
+                    return;
+                }
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Order: " + orderID + " was not found.");
             }
         }
     }
 
 
     public int createNewOrder(Order order) {
+        if(order.customerName.isEmpty() || order.customerName == null){
+            throw new IllegalArgumentException("Name is required.");
+        }
+        if(order.customerID <= 0 ){
+            throw new IllegalArgumentException("Customer-ID has to be greater than zero.");
+        }
+        if(order.orderID != 0){
+            throw new IllegalArgumentException(("Order-ID already excists."));
+        }
+
         orders.add(order);
         return order.orderID = ++orderID;
 
